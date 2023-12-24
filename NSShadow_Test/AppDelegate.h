445b10604f063c9809ab32d50dcd3013e312945f
Gregory Casamento <greg.casamento@gmail.com>
2023-12-24 06:16:03 -0500

    Initial Commit

---
+++ NSShadow_Test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSShadow_Test
+//
+//  Created by Gregory John Casamento on 12/24/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSShadow_Test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSShadow_Test
+//
+//  Created by Gregory John Casamento on 12/24/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
