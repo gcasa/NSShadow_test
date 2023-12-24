40cfc5c0ea557d65b07eaa0627b320dc8af40077
Gregory Casamento <greg.casamento@gmail.com>
2023-12-24 12:58:05 -0500

    Add button and shadow code

--- NSShadow_Test/AppDelegate.m
+++ NSShadow_Test/AppDelegate.m
@@ -9,10 +9,39 @@
 
 @interface AppDelegate ()
 
-@property (strong) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSButton *button;
+
 @end
 
 @implementation AppDelegate
+
+- (void) awakeFromNib
+{
+    NSRect frame = [self.button frame];
+    NSRect shadowFrame = NSInsetRect(frame, 5.0, 5.0);
+    shadowFrame.origin.x += 5.0;
+    shadowFrame.origin.y += 5.0;
+
+    // Definir el color de l'ombra
+    NSColor *shadowColor = [NSColor redColor];
+
+    // Definir l'ombra
+    NSShadow *shadow = [[NSShadow alloc] init];
+    [shadow setShadowColor:shadowColor];
+    [shadow setShadowOffset:NSMakeSize(0, -1)];
+    [shadow setShadowBlurRadius:10];
+
+    // Aplicar l'ombra
+    [NSGraphicsContext saveGraphicsState];
+    [shadow set];
+
+    // Dibuixar l'ombra (Això dibuixa una "caixa" d'ombra al voltant de l'àrea del botó)
+    NSBezierPath *shadowPath = [NSBezierPath bezierPathWithRect:shadowFrame];
+    [[NSColor clearColor] set];
+    
+    [self.button setShadow: shadow];
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
